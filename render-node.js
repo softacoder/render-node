4a38f40f683d2362d6dcf92098d5c509dd351ecd
softacoder <113185323+softacoder@users.noreply.github.com>
2023-07-22 12:02:19 +0100

    commit

--- render-node.js
+++ render-node.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log(`Received request with params: ${JSON.stringify(req.query)}`)
-    res.send({'The best ever is': 'class10}) 
-})
+app.get("/", (req, res) => {
+  console.log(`Received request with params: ${JSON.stringify(req.query)}`);
+  res.send({ "The best ever is": "class10" });
+});
 
 let count = 0;
-app.get('/counter', (req, res) => {
-count = count + 1
-res.send({'currentCount': count })
+app.get("/counter", (req, res) => {
+  count = count + 1;
+  res.send({ currentCount: count });
+});
 
-})
-
-app.listen(3000)
+app.listen(3000);
